7370a8158943112390f2a9e818b89466654fe223
ECecillo <ECecillo>
2020-04-09 15:28:41 +0200

    refonte de la page contact GB, remplacer l'ancienne Contact par la nouvelle

--- js/app.js
+++ js/app.js
@@ -55,3 +55,50 @@ open.addEventListener("click", function(){
         changeIcon = true;
     }
 });
+
+
+$( '.js-input' ).keyup(function() {
+  if( $(this).val() ) {
+     $(this).addClass('not-empty');
+  } else {
+     $(this).removeClass('not-empty');
+  }
+});
+
+/* Check si le contact form est empty ou full */
+
+function checkInputs() {
+  var isValid = true;
+  $('input').filter('[required]').each(function() {
+    if ($(this).val() === '') {
+      $('#sub').prop('disabled', true)
+      isValid = false;
+      return false;
+    }
+  });
+  if(isValid) {$('#sub').prop('disabled', false)}
+  return isValid;
+}
+
+
+/* <!-- Modal --> */
+const submit = document.getElementById('sub');
+var modal = "<div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'>  <h5 class='modal-title' id='exampleModalLabel'>Modal title</h5>  <button type='button' class='close' data-dismiss='modal' aria-label='Close'>    <span aria-hidden='true'>&times;</span>  </button></div><div class='modal-body'>  ...</div><div class='modal-footer'>  <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>  <button type='button class='btn btn-primary'>Save changes</button></div></div></div></div>"
+
+//Enable or disable button based on if inputs are filled or not
+$('input').filter('[required]').on('keyup',function() {
+checkInputs()
+})
+
+checkInputs();
+
+/* Fonction qui renvoie le modal */
+
+if(checkInputs) {
+  document.submit.appendChild(modal);
+}
+else {
+  checkInputs();
+}
+
+
