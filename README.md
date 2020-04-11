a965ccd30726eb04f0d11af8ef329a09af835f95
ECecillo <ECecillo>
2020-04-11 10:45:03 +0200

    ajout de l'auto complete, traduction de tout les jeux

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# TP2 | Vous trouverez ci-joint le template utilisé pour ce site
+
+![logo du site](https://github.com/ECecillo/MemoryWeb/blob/master/img/favicon.ico)
+# Projet | MEMORY
 
 ### Lien vers le site du projet hebergé par github : https://ececillo.github.io/MemoryWeb/.
+
+| Fonctionnalité | Etat |
+|--|--|
+| Thème Noir et blanc |  |
--- js/app.js
+++ js/app.js
@@ -86,27 +86,12 @@ $('input').filter('[required]').on('keyup', function () {
 
 checkInputs();
 
-/* <!-- Modal --> */
-/* const submit = document.getElementById('sub'); */
 
-
-/* var modal = "<div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'>  <h5 class='modal-title' id='exampleModalLabel'>Modal title</h5>  <button type='button' class='close' data-dismiss='modal' aria-label='Close'>    <span aria-hidden='true'>&times;</span>  </button></div><div class='modal-body'>  ...</div><div class='modal-footer'>  <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>  <button type='button class='btn btn-primary'>Save changes</button></div></div></div></div>"
- */
-
-//Enable or disable button based on if inputs are filled or not
-
-
-
-
-/* Fonction qui renvoie le modal */
-
-/* var node = document.createElement("div"); */
-
-/* if (checkInputs) {
-  submit.appendChild(modal);
+function newPage(num) {
+  var url=new Array();
+  url[0]="index_GB.html";
+  url[1]="../index.html";
+  window.location=url[num];
 }
-else {
-  checkInputs();
-} */
 
 
